bb5110a27169fc527fcebf13df628adf706d4995
Letitia Wu <Letitia_wood@icloud.com>
2018-10-04 11:22:48 +0800

    Initial Commit

---
+++ My first App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  My first App
+//
+//  Created by Letitia Wu on 2018/10/4.
+//  Copyright © 2018年 Letitia Wu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
