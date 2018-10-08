8644a5ff8a389bc7c6e415cf914f5d39f670b566
Unknown <Letitia_wood@icloud.com>
2018-10-08 10:19:56 +0800

    now you can add or subtract
    switch

--- My first App/ViewController.swift
+++ My first App/ViewController.swift
@@ -13,20 +13,25 @@ class ViewController: UIViewController {
 //    var buttonCount = 0
     
     @IBOutlet weak var myLabel: UILabel!
-
     @IBOutlet weak var topTextField: UITextField!
-    
     @IBOutlet weak var bottomTextField: UITextField!
-    
+    @IBOutlet weak var additionSwitch: UISwitch!
     
     @IBAction func buttonTapped(_ sender: Any) {
         
         print(topTextField.text!)
         print(bottomTextField.text!)
         
-        var sum = Double(topTextField.text!)! + Double(bottomTextField.text!)!
+        let addition = additionSwitch.isOn
+        if addition {
+            let sum = Double(topTextField.text!)! + Double(bottomTextField.text!)!
+            myLabel.text = "\(topTextField.text!) + \(bottomTextField.text!) = \(sum)"
+        } else {
+            let sum = Double(topTextField.text!)! - Double(bottomTextField.text!)!
+            myLabel.text = "\(topTextField.text!) - \(bottomTextField.text!) = \(sum)"
+        }
         
-        myLabel.text = "\(topTextField.text!) + \(bottomTextField.text!) = \(sum)"
+        
         
   /*
         buttonCount += 1
